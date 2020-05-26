c7bcc43c1f57df8e72d43160a9817e31ec3aca9a
Fannar Gauti Guðmundsson <fannar@dohop.com>
2020-05-26 16:51:55 +0200

    Updates

---
+++ precache-manifest.2c87854de4aeecb127bb982504d9d18b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a0544c8f693a80cca8687c0f1a8956",
+    "url": "/github-organizations/index.html"
+  },
+  {
+    "revision": "c80872bb9948c1abd254",
+    "url": "/github-organizations/static/css/main.85fdbc5e.chunk.css"
+  },
+  {
+    "revision": "61ed8acc7b0464b4aa78",
+    "url": "/github-organizations/static/js/2.ff62a8a6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github-organizations/static/js/2.ff62a8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80872bb9948c1abd254",
+    "url": "/github-organizations/static/js/main.c0398b9c.chunk.js"
+  },
+  {
+    "revision": "a03bfe387aa8ae418954",
+    "url": "/github-organizations/static/js/runtime-main.84d4bf3c.js"
+  },
+  {
+    "revision": "24eda3025c21d1f234865bd1384fb8f2",
+    "url": "/github-organizations/static/media/arrow.24eda302.svg"
+  },
+  {
+    "revision": "d25d9fd159ec1223601f01d9bac392b9",
+    "url": "/github-organizations/static/media/fork.d25d9fd1.svg"
+  },
+  {
+    "revision": "2c56ed7a59d64b910fcb006be6e13343",
+    "url": "/github-organizations/static/media/star.2c56ed7a.svg"
+  }
+]);
